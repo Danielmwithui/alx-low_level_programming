4f1e2efda88b297687118190ba5e7e63ffc70977
Daniel Mwithui <danielmwithui9@gmail.com.com>
2022-07-05 19:44:22 -0700

    hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
-nclude "main.h"
+#include "main.h"
 /**
- * print_alphabet  -> print the lower case  alphabet
- * Return: always return 0
- * */
+*print_alphabet  -> print the lower case  alphabet
+*
+*Return: always return 0
+*/
 void print_alphabet(void)
 {
-	int j;
-	for (j = 'a'; j <= 'z'; j++)
-	{
-		_putchar(j);
-	}
-	_putchar('\n');
+int j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
 }
+_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This is my readme file
+C Functions and Nested Loops readme file
