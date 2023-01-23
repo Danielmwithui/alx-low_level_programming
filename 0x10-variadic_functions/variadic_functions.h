0a871c032f2af8a9ca0e1246582b76b262db811a
root <danielmwithui9@gmail.com>
2023-01-23 07:51:41 -0800

    first commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,19 @@
-#ifndef VARIAFUNC
-#define VARIAFUNC
+#ifndef _VARIADIC_H_
 
-#include <stdarg.h>
-#include <stdio.h>
-/**
- * struct args_t - contain the type and the function
- * @format: the format to print
- * @function: the functions that prints
- * Description: contains the type and the format to print.
- */
-typedef struct args_t
-{
-	char *format;
-	void (*function)();
-} ars_t;
+#define _VARIADIC_H_
+
+
+
+
+
+int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
 
-#endif /* VARIAFUNC */
+#endif
